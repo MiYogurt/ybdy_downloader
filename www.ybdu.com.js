aa95a8bf5acc031a71f9a7122a152c379a7465db
ChenMingMing <belovedyogurt@gmail.com>
2018-07-15 10:10:56 +0800

    Create www.ybdu.com.js

---
+++ www.ybdu.com.js
@@ -0,0 +1,44 @@
+module.exports = load => {
+  const join = load('url-join')
+  return {
+    regexp: /www\.ybdu\.com/gi,
+    opts: {
+      charset: 'gbk'
+    },
+    chapter($, url) {
+      const datas = []
+      $('.mulu_list li').each(function(i, ele) {
+        const self = $(this)
+        const title = self.text().replace(/\s/g, '')
+        const chapter_url = self.find('a').attr('href')
+        datas.push({
+          title,
+          url: join(url, chapter_url)
+        })
+      })
+      return datas
+    },
+    text($) {
+      const trim = sourceString => {
+        const dels = [
+          '加入书签',
+          '加入书架',
+          '推荐投票',
+          '返回书页',
+          '上一页',
+          '返回目录',
+          '下一页',
+          /\s+/gi,
+          /(\－)*/gi
+        ]
+        dels.forEach(delString => {
+          // 纯函数
+          sourceString = sourceString.replace(delString, '')
+        })
+        return sourceString
+      }
+      const sourceString = $('#htmlContent').text()
+      return trim(sourceString)
+    }
+  }
+}
